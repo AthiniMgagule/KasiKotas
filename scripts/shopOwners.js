d01008a814577e5fb948e450671ed3c1038c187f
AthiniMgagule <148635525+AthiniMgagule@users.noreply.github.com>
2025-01-25 14:27:56 +0200

    Add files via upload

--- scripts/shopOwners.js
+++ scripts/shopOwners.js
@@ -1,6 +1,3 @@
-
-
-
 function chatbox(){
     const chatbotIcon = document.getElementById('chatbot-icon');
     const chatbotWindow = document.getElementById('chatbot-window');
@@ -24,3 +21,62 @@ function chatbox(){
 }
 
 document.addEventListener("DOMContentLoaded", chatbox);
+
+
+async function ownersignup() {
+    document.getElementById('ownerRegistration').addEventListener('submit', async function (event) {
+        event.preventDefault();
+    
+        const ownerName = document.getElementById('ownerName').value;
+        const ownerSurname = document.getElementById('ownerSurname').value;
+        const ownerEmail = document.getElementById('ownerEmail').value;
+        const ownerPassword = document.getElementById('ownerPassword').value;
+        const confirmPassword = document.getElementById('confirmOwnerPassword').value;
+        
+        console.log('Name:', ownerName);
+        console.log('Surname', ownerSurname);
+        console.log('Email:', ownerEmail);
+        console.log('Password:', ownerPassword);
+        console.log('Confirm Password:', confirmPassword);
+    
+        if (ownerPassword !== confirmPassword) {
+            alert('Passwords do not match.');
+            return;
+        }
+    
+        const data = { ownerName, ownerSurname, ownerEmail, ownerPassword };
+    
+        try {
+            const response = await fetch('https://kasikotas-api.onrender.com/ownerSignup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+    
+            if (response.ok) {
+                const result = await response.json();
+                alert('User created successfully!');
+                // Redirect to login
+                window.location.href = '#login';
+            } else {
+                const error = await response.text();
+                alert('User already exists, try logging in');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            alert('An unexpected error occurred.');
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", ownersignup);
+
+function signinpage(){
+    const ownerRegistration = document.getElementByID("ownerRegistration");
+    const ownerLogin = document.getElementById("ownerLogin");
+    ownerRegistration.style.display = 'none';
+    ownerLogin.style.display = 'block';
+}
+document.addEventListener("DOMContentLoaded", ownersignup);
