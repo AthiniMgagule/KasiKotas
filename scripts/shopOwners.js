5f327bad060341788ccf2f5b1d4f0fc4fecb5302
AthiniMgagule <148635525+AthiniMgagule@users.noreply.github.com>
2025-01-30 15:28:59 +0200

    Add files via upload

--- scripts/shopOwners.js
+++ scripts/shopOwners.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", chatbox);
 
 
 async function ownersignup() {
-    document.getElementById('ownerRegistration').addEventListener('submit', async function (event) {
+    document.getElementById('ownerForm').addEventListener('submit', async function (event) {
         event.preventDefault();
     
         const ownerName = document.getElementById('ownerName').value;
@@ -74,9 +74,63 @@ async function ownersignup() {
 document.addEventListener("DOMContentLoaded", ownersignup);
 
 function signinpage(){
-    const ownerRegistration = document.getElementByID("ownerRegistration");
+    const ownerRegistration = document.getElementById("ownerRegistration");
     const ownerLogin = document.getElementById("ownerLogin");
     ownerRegistration.style.display = 'none';
     ownerLogin.style.display = 'block';
 }
-document.addEventListener("DOMContentLoaded", ownersignup);
+
+async function ownerlogin(){
+    document.getElementById('ownerLogin').addEventListener('submit', async function(event){
+        event.preventDefault();
+
+        const ownerEmail = document.getElementById('ownerEmail').value;
+        const ownerPassword = document.getElementById('ownerPassword').value;
+
+        console.log('owner login email: ', ownerEmail);
+        console.log("owner pasword login: ", ownerPassword);
+
+        const data = {ownerEmail, ownerPassword};
+
+        try{
+            const response = await fetch('https://kasikotas-api.onrender.com/ownerLogin', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+            if (response.ok) {
+                const result = await response.json();
+                alert('User loggedin successfully!');
+                // Redirect to landing page
+                window.location.href = '#home';
+
+                //now block the display of home, menuManagement, orderManagement, feedbackReview
+                const home = document.getElementById('home');
+                const ownerRegistration = document.getElementById('ownerRegistration');
+                const menuManagement = document.getElementById('menuManagement');
+                const orderManagement = document.getElementById('orderManagement');
+                const feedbackReview = document.getElementById('feedbackReview');
+
+                ownerRegistration.style.display = 'none';
+                home.style.display = 'block';
+                menuManagement.style.display = 'block';
+                orderManagement.style.display = 'block';
+                feedbackReview.style.display = 'block';
+
+                addnewkota();
+                shownotifications();
+
+
+            } else {
+                const error = await response.text();
+                alert(error);
+            }
+        }catch (error) {
+            console.error('Error:', error);
+            alert('An unexpected error occurred.');
+        }
+    });
+}
+document.addEventListener("DOMContentLoaded", ownerlogin);
